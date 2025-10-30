58fd4302eedac007789427d8a4ae6bf1e5babde3
Victoria Arriola <arriolafalkvictoria@gmail.com>
2025-10-30 17:22:05 -0300

    Renombrado archivo de test a minúsculas para pytest

--- Test/Test_Arriola_potencia.py
+++
@@ -1,7 +0,0 @@
-#tests/test_potencia.py
-from Funciones.Funciones_potenciaArriola import potencia
-
-
-def test_potencia():
-  assert potencia(2, 3) == 8
-  assert potencia(5, 0) == 1
--- Test/Test_potenciaArriola.py
+++ Test/Test_potenciaArriola.py
@@ -1,8 +1,6 @@
 #tests/test_potencia.py
-from Funciones.funciones_potenciaArriola import potencia
-
+from Funciones.Arriola_potencia import potencia
 
 def test_potencia():
   assert potencia(2, 3) == 8
   assert potencia(5, 0) == 1
- 
