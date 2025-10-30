223058930f0ab1ad9b1d0189245cb3f572b1f3cb
Victoria Arriola <arriolafalkvictoria@gmail.com>
2025-10-30 16:31:39 -0300

    Corrijo import en Test_potenciaArriola tras renombrar carpeta Funciones

--- Test/Test_potenciaArriola.py
+++ Test/Test_potenciaArriola.py
@@ -1,6 +1,8 @@
 #tests/test_potencia.py
-from funciones.potencia import potencia
+from Funciones.funciones_potenciaArriola import potencia
+
 
 def test_potencia():
   assert potencia(2, 3) == 8
   assert potencia(5, 0) == 1
+ 
