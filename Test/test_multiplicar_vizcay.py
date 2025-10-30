d0329d0454cf547cd6efe853feb5cbcac6ad8d71
Lucas Vizcay <lucasvizcay62gmail.com>
2025-10-30 15:30:07 -0300

    Agrego función multiplicar_vizcay

--- Test/test_multiplicar_vizcay.py
+++ Test/test_multiplicar_vizcay.py
@@ -1,6 +1,6 @@
 # tests/test_multiplicar_vizcay.py
 
-from funciones.multiplicar_vizcay import multiplicar_vizcay
+from Funciones.multiplicar_vizcay import multiplicar_vizcay
 
 def test_multiplicar_vizcay():
     assert multiplicar_vizcay(3, 4) == 12
